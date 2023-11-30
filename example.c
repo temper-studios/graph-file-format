d819871c12643786555fe21cdbb5459a121e7894
temper-studios <115193648+temper-studios@users.noreply.github.com>
2023-11-30 18:15:06 +0000

    Update example.c

--- example.c
+++ example.c
@@ -1,71 +1,205 @@
-#include "graph.h"
+#include "graph_file_format.h"
 
 int main() {
 
-	typedef struct MyStruct {
+	struct InnerStruct {
 		double a;
-		graph_u64 b;
-		graph_s64 c;
-		char str[64];
-	} MyStruct;
+		int64_t b;
+		int32_t list[4];
+	};
 
-	MyStruct myStructSave;
-	myStructSave.a = 3.14;
-	myStructSave.b = 101;
-	myStructSave.c = -13;
-	strncpy(myStructSave.str, "hello", 5);
-	myStructSave.str[5] = '\0';
-
+	struct MyStruct {
+		uint32_t a;
+		float b;
+		float xyz[3];
+		struct InnerStruct inner;
+		char str[4];
+		uint64_t c;
+	};
 
 	{
-		graph_saver saver;
+		struct MyStruct myStruct;
+		myStruct.a = 10;
+		myStruct.b = 3.14f;
+		myStruct.xyz[0] = 1.0;
+		myStruct.xyz[1] = 2.0;
+		myStruct.xyz[2] = 3.0;
+		myStruct.str[0] = 'S';
+		myStruct.str[1] = 't';
+		myStruct.str[2] = 'r';
+		myStruct.str[3] = '\0';
+		myStruct.c = 63;
 
-		// because we use the exit callback we don't need to check for errors since we will exit on error.
-		graph_saver_start(&saver, graph_error_context_default_exit_callback);
+		myStruct.inner.a = -1.32;
+		myStruct.inner.b = -232;
+		myStruct.inner.list[0] = 0;
+		myStruct.inner.list[1] = 1;
+		myStruct.inner.list[2] = 2;
+		myStruct.inner.list[3] = 3;
 
-		graph_saver_node *nodeMyStruct = graph_save_node(&saver, NULL, "MyStruct");
+		gf_Saver saver;
+		gf_InitSaver(&saver, NULL);
 
-		graph_saver_node *nodeA = graph_save_node(&saver, nodeMyStruct, "a");
-		graph_save_double(&saver, nodeA, myStructSave.a);
+		FILE *file = fopen("data.gf", "wb");
+		if (!file) {
+			puts("could not open file");
+			return 0;
+		}
 
-		graph_saver_node *nodeB = graph_save_node(&saver, nodeMyStruct, "b");
-		graph_save_u64(&saver, nodeB, myStructSave.b);
+		gf_SaveStartList(&saver, file, "MyStruct");
+		{
+			gf_SaveVariableU32(&saver, file, "a", &myStruct.a);
+			gf_SaveVariableF32(&saver, file, "b", &myStruct.b);
+			gf_SaveVariableVec3(&saver, file, "xyz", &myStruct.xyz[0], &myStruct.xyz[1], &myStruct.xyz[2]);
 
-		graph_saver_node *nodeC = graph_save_node(&saver, nodeMyStruct, "c");
-		graph_save_s64(&saver, nodeC, myStructSave.c);
+			gf_SaveStartList(&saver, file, "Inner");
+			{
+				gf_SaveVariableF64(&saver, file, "a", &myStruct.inner.a);
+				gf_SaveVariableS64(&saver, file, "b", &myStruct.inner.b);
+				gf_SaveArrayS32(&saver, file, "list", myStruct.inner.list, 4);
+			}
+			gf_SaveEndList(&saver, file);
 
-		graph_saver_node *nodeStr = graph_save_node(&saver, nodeMyStruct, "str");
-		graph_save_string_span(&saver, nodeStr, myStructSave.str, graph_utility_string_length(myStructSave.str));
+			gf_SaveVariableString(&saver, file, "str", myStruct.str);
+			gf_SaveVariableU64(&saver, file, "c", &myStruct.c);
 
-		graph_save_to_file(&saver, "example.graph");
+		}
+		gf_SaveEndList(&saver, file);
 
-		graph_saver_finish(&saver);
+		fclose(file);
 	}
 
-	MyStruct myStructLoad;
+	struct MyStruct myStruct;
 	{
-		graph_loader loader;
+		gf_Loader loader;
+		int result = 0;
 
-		graph_load_file(&loader, "example.graph", graph_error_context_default_exit_callback);
+		result = gf_LoadFromFile(&loader, "data.gf", NULL);
+		if (!result) {return 0; }
+		{
+			gf_LoaderNode *root = gf_GetRoot(&loader);
 
-		// Access the implicit root node of the loader (where our data has been saved);
-		graph_loader_node *root = graph_loader_get_root(&loader);
+			gf_LoaderNode *nodeMyStruct = gf_GetChild(&loader, root);
+			{
+				gf_LoaderNode *child = gf_GetChild(&loader, nodeMyStruct);
+				gf_LoadVariableU32(&loader, child, &myStruct.a);
 
-		graph_loader_node *nodeMyStruct = graph_loader_get_child(&loader, root);
+				child = gf_GetNext(&loader, child);
+				gf_LoadVariableF32(&loader, child, &myStruct.b);
 
-		graph_loader_node *nodeA = graph_loader_get_child(&loader, nodeMyStruct);
-		graph_loader_node_to_double(&loader, nodeA->children, &myStructLoad.a);
+				child = gf_GetNext(&loader, child);
+				gf_LoadVariableVec3(&loader, child, &myStruct.xyz[0], &myStruct.xyz[1], &myStruct.xyz[2]);
 
-		graph_loader_node *nodeB = graph_loader_get_next(&loader, nodeA);
-		graph_loader_node_to_u64(&loader, nodeB->children, &myStructLoad.b);
+				child = gf_GetNext(&loader, child);
+				{
+					gf_LoaderNode *childInner = gf_GetChild(&loader, child);
+					gf_LoadVariableF64(&loader, childInner, &myStruct.inner.a);
 
-		graph_loader_node *nodeC = graph_loader_get_next(&loader, nodeB);
-		graph_loader_node_to_s64(&loader, nodeC->children, &myStructLoad.c);
+					childInner = gf_GetNext(&loader, childInner);
+					gf_LoadVariableS64(&loader, childInner, &myStruct.inner.b);
 
-		graph_loader_node *nodeStr = graph_loader_get_next(&loader, nodeC);
-		graph_loader_node_to_string(&loader, nodeStr->children, myStructLoad.str, 64);
+					childInner = gf_GetNext(&loader, childInner);
+					gf_LoadArrayS32(&loader, childInner, myStruct.inner.list, 4);
+				}
 
-		graph_unload(&loader);
+				child = gf_GetNext(&loader, child);
+				gf_LoadVariableString(&loader, child, myStruct.str, 4);
+
+				child = gf_GetNext(&loader, child);
+				gf_LoadVariableU64(&loader, child, &myStruct.c);
+			}
+
+		}
+		gf_Unload(&loader);
+	}
+
+	{
+		struct MyStruct myStruct;
+		gf_Loader loader;
+		gf_LoadFromFile(&loader, "data.gf", NULL);
+		{
+			gf_LoaderNode *root = gf_GetRoot(&loader);
+
+			gf_LoaderNode *nodeMyStruct = gf_FindFirstChild(&loader, root, "MyStruct");
+
+			gf_LoaderNode *nodeA = gf_FindFirstChild(&loader, nodeMyStruct, "a");
+			gf_LoadVariableU32(&loader, nodeA, &myStruct.a);
+
+			gf_LoaderNode *nodeB = gf_FindFirstChild(&loader, nodeMyStruct, "b");
+			gf_LoadVariableF32(&loader, nodeB, &myStruct.b);
+
+			gf_LoaderNode *nodeXYZ = gf_FindFirstChild(&loader, nodeMyStruct, "xyz");
+			gf_LoadVariableVec3(&loader, nodeXYZ, &myStruct.xyz[0], &myStruct.xyz[1], &myStruct.xyz[2]);
+
+			gf_LoaderNode *nodeInner = gf_FindFirstChild(&loader, nodeMyStruct, "Inner");
+			{
+				gf_LoaderNode *nodeInnerA = gf_FindFirstChild(&loader, nodeInner, "a");
+				gf_LoadVariableF64(&loader, nodeInnerA, &myStruct.inner.a);
+
+				gf_LoaderNode *nodeInnerB = gf_FindFirstChild(&loader, nodeInner, "b");
+				gf_LoadVariableS64(&loader, nodeInnerB, &myStruct.inner.b);
+
+				gf_LoaderNode *nodeInnerList = gf_FindFirstChild(&loader, nodeInner, "list");
+				gf_LoadArrayS32(&loader, nodeInnerList, myStruct.inner.list, 4);
+			}
+
+			gf_LoaderNode *nodeStr = gf_FindFirstChild(&loader, nodeMyStruct, "str");
+			gf_LoadVariableString(&loader, nodeStr, myStruct.str, 4);
+
+			gf_LoaderNode *nodeC = gf_FindFirstChild(&loader, nodeMyStruct, "c");
+			gf_LoadVariableU64(&loader, nodeC, &myStruct.c);
+		}
+		gf_Unload(&loader);
+
+	}
+
+	{
+		const char *str = "a b c d e ";
+		gf_Loader loader;
+		int result = gf_LoadFromBuffer(&loader, str, gf_StringLength(str), NULL);
+		gf_Unload(&loader);
+	}
+	{
+		const char *str = "a /*";
+		gf_Loader loader;
+		int result = gf_LoadFromBuffer(&loader, str, gf_StringLength(str), NULL);
+		gf_Unload(&loader);
+	}
+	{
+		const char *str = "a \"";
+		gf_Loader loader;
+		int result = gf_LoadFromBuffer(&loader, str, gf_StringLength(str), NULL);
+		gf_Unload(&loader);
+	}
+	{
+		const char *str = "1, 1.0";
+		gf_Loader loader;
+		int result = gf_LoadFromBuffer(&loader, str, gf_StringLength(str), NULL);
+		gf_Unload(&loader);
+	}
+
+	{
+		const char *str =
+			"MyStruct {\n"
+			"  a { 3.14000 }\n"
+			"  b { 101 }\n"
+			"  c { -13 }\n"
+			"  str { \"hello\" }\n"
+			"}\n";
+
+		gf_Loader loader;
+		int result = gf_LoadFromBuffer(&loader, str, gf_StringLength(str), NULL);
+		gf_Unload(&loader);
+	}
+
+	{
+		const char *str =
+			"1.0\n"
+			"2.0\n"
+			"Hello\n";
+		gf_Loader loader;
+		int result = gf_LoadFromBuffer(&loader, str, gf_StringLength(str), NULL);
+		gf_Unload(&loader);
 	}
 
 	return 1;
